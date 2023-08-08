379e56ef67ba31cdb08ffa5652128863d5fb9997
Jeroen <jeroen@illusivegames.com>
2023-08-08 20:46:40 +0200

    hardcoded connections

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -27,9 +27,22 @@ int main()
     node* nodeA = new node();
     node* nodeB = new node();
     node* nodeC = new node();
+    node* nodeD = new node();
+    node* nodeE = new node();
+    node* nodeF = new node();
+    node* nodeG = new node();
 
-    nodeA->connectInOneDirectionTo(nodeB);
-    nodeA->connectInBothDirectionsTo(nodeC);
+    nodeA->connectInBothDirectionsTo(nodeB);
+    nodeB->connectInOneDirectionTo(nodeC);
+    nodeE->connectInOneDirectionTo(nodeA);
+    nodeB->connectInOneDirectionTo(nodeE);
+    nodeD->connectInOneDirectionTo(nodeE);
+    nodeG->connectInOneDirectionTo(nodeE);
+    nodeD->connectInBothDirectionsTo(nodeG);
+    nodeF->connectInOneDirectionTo(nodeG);
+    nodeF->connectInOneDirectionTo(nodeD);
+    nodeF->connectInOneDirectionTo(nodeA);
+    nodeF->connectInOneDirectionTo(nodeC);
 
     cout << "\nNodeA eges: " << nodeA->size;
     cout << "\nNodeB eges: " << nodeB->size;
