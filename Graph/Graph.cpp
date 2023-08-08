591f63d5b5a70fff990d98c3097c98009e97d6b7
Jeroen <jeroen@illusivegames.com>
2023-08-08 20:40:52 +0200

    node test

--- Graph/Graph.cpp
+++ Graph/Graph.cpp
@@ -2,9 +2,38 @@
 
 using namespace std;
 
+const int SIZE = 5;
+
+struct node
+{
+    node* edges[SIZE] = {};
+    int size = 0;
+
+    void connectInOneDirectionTo(node* node)
+    {
+        edges[size] = node;
+        size++;
+    }
+
+    void connectInBothDirectionsTo(node* node)
+    {
+        connectInOneDirectionTo(node);
+        node->connectInOneDirectionTo(this);
+    }
+};
+
 int main()
 {
-    cout << "\nHello World";
+    node* nodeA = new node();
+    node* nodeB = new node();
+    node* nodeC = new node();
+
+    nodeA->connectInOneDirectionTo(nodeB);
+    nodeA->connectInBothDirectionsTo(nodeC);
+
+    cout << "\nNodeA eges: " << nodeA->size;
+    cout << "\nNodeB eges: " << nodeB->size;
+    cout << "\nNodeC eges: " << nodeC->size;
 
     return 0;
 }
